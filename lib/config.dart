47b02783ed7bc2a88a28c37d33da7ea0b5274cd1
lukamihic <luka.mihic@edu.fit.ba>
2024-11-13 00:01:08 +0100

    change from localhost to docker api

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
-const String apiUri = 'http://localhost:5000/api/';
+const String apiUri = 'http://10.0.2.2:5000/api/';
 const String mqUri = '';
 const String stripeUri = 'https://api.stripe.com/v1/';
 const String stripeApiKey =
